c1cb4df7f849749378c475b3fcd7f7596e81553d
Sockstah <Sockstah@users.noreply.github.com>
2017-06-18 12:02:06 +0200

    Added smug command to post smug Lain as well as the image for the command.

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ async def roll(ctx, member: discord.Member = None):
         number = random.randint(1, 20)
 
     await bot.say('{0.mention}'.format(member) + ' ' + str(number))
+    
+@bot.command(pass_context = True)
+async def smug(ctx):
+    """posts smug anime girls"""
+    await bot.send_file(ctx.message.channel, 'smug.jpg')
 
 token_file = open("token",'r')
 token = token_file.read().replace("\n", "")

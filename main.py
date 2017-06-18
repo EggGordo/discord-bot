86c553276f20a7aa5dfce3f510864e58eef79ce3
Keith Henderson Jr <keithhendersonjr@gmail.com>
2017-06-18 05:44:58 -0400

    intial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import random
+import sys
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='$', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command(pass_context=True)
+async def roll(ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.message.author
+        number = random.randint(1, 20)
+
+    await bot.say('{0.mention}'.format(member) + ' ' + str(number))
+
+token_file = open("token",'r')
+token = token_file.read().replace("\n", "")
+bot.run(token)

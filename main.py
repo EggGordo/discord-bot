2dcf4a804751d82ac6fc546c5ef9fd6c929f90b7
Sockstah <Sockstah@users.noreply.github.com>
2017-06-19 21:11:13 +0200

    Greatly expanded video playback feature set.

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 import random
 import sys
 import smug_anime
-import asyncio
+import music
 
 description = """An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here."""
 bot = commands.Bot(command_prefix='$', description=description)
+bot.add_cog(music.Music(bot))
 
 @bot.event
 async def on_ready():
@@ -34,21 +35,6 @@ async def roll(ctx, dice : str):
 async def smug(ctx):
     """posts smug anime girls"""
     await bot.send_file(ctx.message.channel, "images/" + smug_anime.random_pick("images"))
-
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
-
-@bot.command(pass_context = True)
-async def play(ctx, url):
-    """play videos"""
-    voice = await bot.join_voice_channel(ctx.message.author.voice_channel)
-    player = await voice.create_ytdl_player(url)
-    player.start()
 
 token_file = open("token")
 token = token_file.read().replace('\n', '')
